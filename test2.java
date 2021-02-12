def0b4c5aadc3101a992486a3a5cd146af0d16fb
Leduin-Abel <l.abel.bejaranop@gmail.com>
2021-02-11 21:36:35 -0500

    ejercicio 2 java

---
+++ test2.java
@@ -0,0 +1,13 @@
+public class test2 {
+    public static void main(String[] args){
+
+        int sum1=74;
+        int sum2=36; 
+        int result=sum1+sum2; 
+
+        System.out.println(result);
+
+    }
+
+
+}
