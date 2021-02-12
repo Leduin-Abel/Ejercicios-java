f9cf57011c1ffb512018e16e9d62cb8421c30327
Leduin-Abel <l.abel.bejaranop@gmail.com>
2021-02-11 21:41:12 -0500

    ejercicio 3 java

---
+++ test3.java
@@ -0,0 +1,13 @@
+public class test3 {
+    public static void main(String[] args){
+
+        float div1=50;
+        float div2=3; 
+        int result=div1/div2; 
+
+        System.out.println(result);
+
+    }
+
+
+}
