4f6b980a10352b9ae8936e32a7fb760e9e212b51
Leduin-Abel <l.abel.bejaranop@gmail.com>
2021-02-11 22:04:06 -0500

    ejercicio 4 java

---
+++ test4.java
@@ -0,0 +1,16 @@
+public class test4 {
+    public static void main(String[] args){
+
+        float a=-5+8*6;
+        float b=(55+9)%9;
+        float c=20+-3*5/8;
+        float d=5+15/3*2-8%3;
+        System.out.printf("a:%f",a);
+        System.out.printf("\nb:%f",b);
+        System.out.printf("\nc:%f",c);
+        System.out.printf("\nd:%f",d);
+
+    }
+
+
+}
