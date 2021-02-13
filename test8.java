65ed57da8de3f7f1d0d490d802038703cc547ef9
Leduin-Abel <l.abel.bejaranop@gmail.com>
2021-02-12 20:45:51 -0500

    ejercicio 8 java

---
+++ test8.java
@@ -0,0 +1,21 @@
+
+
+public class test8 {
+    public static void main(String[] args){
+   
+        System.out.print( "   J     a    v      v   a\n");
+        System.out.print( "   J   a   a   v    v  a   a\n");
+        System.out.print( "J  J  aaaaaaa   V V   aaaaaaa\n");
+        System.out.print( " JJ  a       a   V   a        a\n");
+
+
+
+  
+    
+
+
+
+    }
+
+
+}
