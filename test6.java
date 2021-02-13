d2045b55da7318056ee967122005b8483a583867
Leduin-Abel <l.abel.bejaranop@gmail.com>
2021-02-12 20:10:48 -0500

    ejercicio 6 java

---
+++ test6.java
@@ -0,0 +1,27 @@
+
+
+public class test6 {
+    public static void main(String[] args){
+        
+        int num1=125;
+        int num2=24;
+        int sum=num1+num2;
+        int dif=num1-num2;
+        int mul=num1*num2;
+        float div=num1/num2;
+        float mod=num1%num2;
+
+        System.out.println(num1 + "+" + num2 + "=" + sum);
+        System.out.println(num1 + "-" + num2 + "=" + dif);
+        System.out.println(num1 + "x" + num2 + "=" + mul);
+        System.out.println(num1 + "/" + num2 + "=" + div);
+        System.out.println(num1 + "mod" + num2 + "=" + mod);
+  
+    
+
+
+
+    }
+
+
+}
