a199257e4b7aeb6608824a8a3bc51275bcfcd54a
Leduin-Abel <l.abel.bejaranop@gmail.com>
2021-02-13 20:29:18 -0500

    ejercicio 12 java,lanza la exepción InputMismatchException, debería funcionar correctamente, lo probé en una consola en línea y funciona en esa pero no en la consola local del PC

---
+++ test11.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+
+
+public class test11 {
+    public static void main(String[] args){
+        
+        Scanner input =new Scanner (System.in);
+
+        System.out.print("Ingrese el radio ");
+        double radio=input.nextDouble();
+
+        System.out.println("El perimetro es:" + 2*Math.PI*radio);
+
+
+        System.out.println("El area es:" + Math.PI*Math.pow(radio, 2));
+
+        input.close();
+  
+    
+
+
+
+    }
+
+
+}
