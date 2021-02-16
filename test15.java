44b0b95af63930264c8544520bbd422481d5abe9
Leduin-Abel <l.abel.bejaranop@gmail.com>
2021-02-16 18:35:22 -0500

    ejercicio 15; 17 conversión de binario

---
+++ test15.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class test15{
+    public static void main(String[] args){
+        
+       int var1=13;
+       int var2=15;
+       int temp=var1;
+       System.out.println(var1 + " and" + var2);
+        var1=var2;
+
+        var2=temp;
+
+        System.out.println(var1 + " and" + var2);
+    
+
+
+
+    }
+
+
+}
