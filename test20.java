eb5af3267985cdd0fce893b77575afb8d18c7f64
Leduin-Abel <l.abel.bejaranop@gmail.com>
2021-03-04 15:31:51 -0500

    ejercicio 20 funcional

--- test20.java
+++ test20.java
@@ -2,56 +2,26 @@ import java.util.Scanner;
 
 public class test20{
     public static void main(String[] args){
-   
-        int numero;
 
-        int i=0;
-        Integer[] hexa1 = new Integer[20];
-        String[] hexa2={};
-        String A="A",B="B",C="C",D="D",E="E",F="F";
-        Scanner entrada = new Scanner(System.in);
-
-            System.out.print("Introduce un numero: ");                                                
-            numero = entrada.nextInt();
-            
-
-            while(numero >=16){
-                hexa1[i++]=numero%16;
-                numero=numero/16;
-            }
-            hexa1.toString();
-    
-            do{
-               
-                if(hexa1[i]==10){
-                    hexa2[i]=A;
-                }
-                else if(hexa1[i]==11){
-                    hexa2[i]=B;
-                }
-                else if(hexa1[i]==12){
-                    hexa2[i]=C;
-                }
-                else if(hexa1[i]==13){
-                    hexa2[i]=D;
-                }
-                else if(hexa1[i]==14){
-                    hexa2[i]=E;
-                }
-                else if(hexa1[i]==15){
-                    hexa2[i]=F;
-                }
-                i--;
-
-            }while(i>=0);
-
-
-                System.out.print(hexa2);
-            
-
-            entrada.close();
-    }
-
-    }
-
-
+        int dec_num, rem;
+        String hexdec_num="";
+        
+        
+        char hex[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        
+        Scanner in = new Scanner(System.in);
+		
+        System.out.print("Input a decimal number: ");
+       // dec_num = in.nextInt();
+		dec_num = 15;
+        while(dec_num>0)
+        {
+            rem = dec_num%16;
+            System.out.print("rem: " + rem + "\n");
+            System.out.print("hex[rem]: " + hex[rem] + "\n");
+            hexdec_num = hex[rem] + hexdec_num;
+            System.out.print("hexdec_num: " + hexdec_num + "\n");
+            dec_num = dec_num/16;
+        }
+        System.out.print("Hexadecimal number is : "+hexdec_num+"\n");    
+    }}
